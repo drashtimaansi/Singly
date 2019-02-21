66a823371c831db7f1b16fe8a983e5d9c901d7c3
hsdamak <hmsi13rcb@gmail.com>
2019-02-21 08:46:40 +0530

    add method:main, create instance for class:SinglyLinkedList linkedList

--- src/main/SinglyLinkedList.java
+++ src/main/SinglyLinkedList.java
@@ -5,6 +5,9 @@ public class SinglyLinkedList {
     private Node head = null;
     private int size = 0;
 
+    // method to insert a new node at the head
+
+
 
 private static class Node{
 private int data;
@@ -20,4 +23,11 @@ private int data;
         this.next = next;
     }
 }
+
+    public static void main(String[] args) {
+    SinglyLinkedList linkedList = new SinglyLinkedList();
+
+    }
+
+
 }
