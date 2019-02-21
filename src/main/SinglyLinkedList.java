d01035e4bd6181c9de731e1c11183750aecc1859
hsdamak <hmsi13rcb@gmail.com>
2019-02-21 08:28:52 +0530

    add constructor for data

--- src/main/SinglyLinkedList.java
+++ src/main/SinglyLinkedList.java
@@ -5,5 +5,10 @@ public class SinglyLinkedList {
 private static class Node{
 private int data;
     private Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
 }
 }
