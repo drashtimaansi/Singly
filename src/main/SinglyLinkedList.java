c6ebcf3fba6cd6177000a5b898f0dcfbd6e3e530
hsdamak <hmsi13rcb@gmail.com>
2019-02-27 09:14:33 +0530

    after some updation and adding remove method

--- src/main/SinglyLinkedList.java
+++ src/main/SinglyLinkedList.java
@@ -6,19 +6,71 @@ public class SinglyLinkedList {
     private int size = 0;
 
     // method to insert a new node at the head
-public void insert(int value){
-   head = new Node(value, head);
-
-  /* Node node = new Node(value, head);
+private void insertHead(int value){
+   this.head = new Node(value, this.head);
+   /* Node node = new Node(value, head);
    head = node;*/
-
   size++;
 }
 
 //method to insert a new node after a node
-public void insert(int value, Node node ){
+private void insertAfter(int value, Node node ){
    node.next = new Node(value, node.next);
     size++;
+}
+
+public void insert(int value){
+    if(head == null){
+        insertHead(value);
+    }else{
+        Node temp = this.head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        insertAfter(value, temp);
+    }
+}
+
+private  int removeHead(){
+    int response = -1;
+    Node temp = this.head;//copy of node
+    if(head != null){
+        this.head = this.head.next;
+    }
+    if(temp != null){
+        size--;
+        response = temp.data;
+    }
+    return response;
+}
+
+private int removeAfter(Node node){
+    int response = -1;
+    Node temp=node.next;
+
+    if(temp != null){
+        response = temp.data;
+        node.next=temp.next;
+        size--;
+    }
+return response;
+}
+
+public int remove(int data){
+    int response = -1;
+    Node temp = this.head;
+    if(temp.data == data){
+        removeHead();
+    }else{
+        while(temp != null){
+            if(temp.data == data){
+                                response=removeAfter(temp);
+                break;
+            }
+        temp =temp.next;
+        }
+    }
+    return response;
 }
 
 
@@ -57,21 +109,26 @@ private int data;
 
     public static void main(String[] args) {
     SinglyLinkedList linkedList = new SinglyLinkedList();
+    linkedList.insert(18);
+        System.out.println(linkedList);
         for (int i = 0; i < 5; i++) {
             linkedList.insert(i+1);
         }
 
-        Node temp = linkedList.head;
+
+       /* Node temp = linkedList.head;
         while(temp != null){
             if(temp.data == 5){
-                linkedList.insert(42,temp);
+                linkedList.insertAfter(42,temp);
                 break;
             }
             temp=temp.next;
-        }
-
+        }*/
+         linkedList.remove(1);
 
         System.out.println(linkedList);
+        System.out.println(linkedList.size);
+
 
     }
 
