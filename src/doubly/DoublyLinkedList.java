c090edcef0fd9ce57b4d63f1dc668879e04cd425
hsdamak <hmsi13rcb@gmail.com>
2019-02-28 09:54:17 +0530

    doubly linked list

--- src/doubly/DoublyLinkedList.java
+++ src/doubly/DoublyLinkedList.java
@@ -1,4 +1,4 @@
-/*package doubly;
+package doubly;
 
 public class DoublyLinkedList {
     private static class Node {
@@ -13,8 +13,6 @@ public class DoublyLinkedList {
 
         public Node(int data) {
             this.data = data;
-            this.prev = null;
-            this.next = null;
         }
     }
 
@@ -25,34 +23,134 @@ public class DoublyLinkedList {
     @Override
     public String toString() {
         StringBuilder response = new StringBuilder();
-        response.append("[");
 
-        Node temp = this.head;
-        while (temp != null) {
-            response.append(temp.data);
-            if (temp.next != null) {
-                response.append(" ==> ");
+        if (size == 0) {
+            response.append("[]");
+        } else {
+            response.append("[");
+            Node temp = this.head;
+            while (temp != null) {
+                response.append(temp.data);
+
+                if (temp.next != null) {
+                    response.append(" ==> ");
+                }
+                temp = temp.next;
             }
-            temp = temp.next;
-        }
 
-        response.append("]");
+            response.append("]");
+            }
         return response.toString();
 
     }
 
-    private  void insertHead(int data){
+    public void insertHead(int data){
 //        Node newNode = new Node(data,null,this.head);
 //        this.head=newNode; OR
-        this.head = new Node(data,null,this.head);
-        size++;
-
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+        }else{
+            newNode.next = head;
+            newNode.prev = head.prev;
+            head.prev =newNode;
+            head = newNode;
+        }
+      size++;
 //
     }
 
+    public void insert(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            insertHead(data);
+        }else{
+            Node temp = head;
+            while(temp.next!=null){
+                temp=temp.next;
+            }
+            newNode.next=temp.next;
+            newNode.prev=temp;
+            temp.next = newNode;
+            tail = newNode;
+            size++;
+        }
+    }
+
+//deletes only last element
+    public int delete(){
+        int response =-1;
+        if(head != null){
+            Node temp = head;
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            response = temp.data;
+            temp = temp.prev;
+            temp.next = null;
+            size--;
+        }
+        return response;
+    }
+
+    public int removeHead(){
+        int response =-1;
+
+        if(head != null){
+            Node temp = head.next;
+            response=temp.data;
+            temp.prev =null;
+            head = temp;
+
+        }
+
+        return response;
+    }
+
+    public int removeAfter(Node node){
+        int response = -1;
+
+        if(node.next != null){
+            Node temp = node.next;
+            response = temp.data;
+
+            node.next = temp.next;
+            temp.next.prev = temp.prev;
+
+            size--;
+        }
+
+    return response;
+    }
+
+    public int remove(int data){
+        int response = -1;
+
+        if(head.data == data){
+            removeHead();
+        }else{
+            Node temp = head;
+            while (temp.next != null){
+                if(temp.next.data == data){
+                    removeAfter(temp);
+                    break;
+                }
+                temp = temp.next;
+            }
+        }
+        return response;
+    }
 
     public static void main(String[] args) {
         DoublyLinkedList linkedList = new DoublyLinkedList();
         System.out.println(linkedList);
+        for (int i=0;i<5;i++){
+            linkedList.insert(i+1);
+        }
+        linkedList.removeHead();
+       int delete = linkedList.delete();
+        System.out.println(delete);
+        System.out.println(linkedList);
+
     }
-}*/
+}
